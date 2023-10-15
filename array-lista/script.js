1281e36b11555dfa82b960143c6220dff7e8f950
Vbortone99 <bortonevincenzo99@gmail.com>
2023-10-15 22:36:14 +0200

    for

--- array-lista/script.js
+++ array-lista/script.js
@@ -1,3 +1,13 @@
 const fruitList = ['banana','mela','pera','ciliegia','arancia','mandarino','cocomero','limone','fragola'];
-fruitList.push('pesca')
-console.log(fruitList);
+fruitList.push('pesca');
+
+for(let i = 0; i < fruitList.lenght; i++){
+    if(i === fruitList[6]){
+        console.log(i);
+        console.log(fruitList)
+    }else{
+        let fruit_msgContainer=document.getElementById('fruit-msgContainer');
+        fruit_msgContainer.innerHTML= "Oh no, devo uscire a comprare il cocomero!"
+    }
+}
+
